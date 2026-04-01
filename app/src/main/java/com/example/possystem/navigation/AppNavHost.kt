399fc32dc9d2b3eaf1955dcc4caa8f9957acb2cd
melchymatw_a11y <melchymat.w@gmail.com>
2026-04-01 15:29:17 +0300

    first commit

---
+++ app/src/main/java/com/example/possystem/navigation/AppNavHost.kt
@@ -0,0 +1,20 @@
+package com.example.possystem.navigation
+
+import android.R
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.possystem.ui.theme.screens.login.LoginScreen
+import com.example.possystem.ui.theme.screens.register.RegisterScreen
+
+
+@Composable
+fun AppNavHost(navController: NavHostController = rememberNavController(),
+               startDestination: String = ROUTE_REGISTER){
+    NavHost(navController,startDestination= startDestination){
+        composable(ROUTE_REGISTER) { RegisterScreen(navController)}
+        composable(ROUTE_LOGIN) { LoginScreen(navController) }
+    }
+}
